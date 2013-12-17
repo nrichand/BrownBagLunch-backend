f3fedf0c82ffad06148048d51a772acbc056ddfd
Francois Vilain <francois.vilain@carboatmedia.fr>
2013-12-17 16:03:53 +0100

    No DB Reset

--- app/db.js
+++ app/db.js
@@ -2,25 +2,6 @@
 
 var mongoose = require('mongoose');
 
-function createResetAll(models,finalCallback){
-
-	function createReset(model,thisCallback){
-		var f = function(err){
-			if(err){
-				finalCallback(err);
-			}
-			model.remove(thisCallback);
-		};
-		return f;
-	}
-	var lastMethod= finalCallback;
-	
-	models.forEach(function(model){
-		var thisRemove = createReset(model, lastMethod);
-		lastMethod= thisRemove;
-	});
-	return lastMethod;
-}
 
 function create(dbUrl, options, models){
 	
@@ -31,12 +12,7 @@ function create(dbUrl, options, models){
 		},
 		disconnect :  function (callback) {
 			mongoose.disconnect(callback);
-		},
-		resetDb: function (callback) {
-			var allReset = createResetAll(models, callback);
-			allReset();
-			return ;
-		}	
+		}
 	}
 	return myDb;
 }
