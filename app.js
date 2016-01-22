72ec3825a5df7643a9cda7a7b5b3d2c088bc6e70
Nathaniel Richand <nrichand@gmail.com>
2016-01-22 16:47:07 +0100

    add openshift conf

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+  app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
